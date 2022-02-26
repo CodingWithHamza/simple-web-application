b1ead17d0d176826acde00f29c40f933cc3f56df
Hamza Manzoor <codingwithhamza@gmail.com>
2022-02-26 06:03:16 +0500

    First Commit

---
+++ src/test/java/com/example/simlewebapplication/SimleWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simlewebapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimleWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
